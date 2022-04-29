8e2f9fe9f327000b8596931316a3e6b7b879d9c7
Guillermo Vicente <84589575+guillermovicenteprieto@users.noreply.github.com>
2022-04-28 21:37:00 -0300

    entrega_workingProgress

--- index.js
+++ index.js
@@ -8,20 +8,5 @@ const PORT = process.env.PORT
 const urlBase = process.env.DB
 
 if (process.env.AMBIENTE === "mongo") mongoose.connect(urlBase)
-// if (process.env.AMBIENTE === "firebase") admin.initializeApp({
-//     credential: admin.credential.cert(serviceAccount),
-//     databaseURL: "http://ecommerce-backend-262b9.firebaseio.com"
-// })
-
-
-
-
-//mongoose.connect(urlBase);
-
-
-// admin.initializeApp({
-//     credential: admin.credential.cert(serviceAccount),
-//       databaseURL: "http://ecommerce-backend-262b9.firebaseio.com"
-//   });
 
 app.listen(PORT, () => console.log(`http://localhost:${PORT}`))
