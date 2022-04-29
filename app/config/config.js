df01fe69c23a5b87434d34910794ff8f4bd93de1
Guillermo Vicente <84589575+guillermovicenteprieto@users.noreply.github.com>
2022-04-28 21:39:34 -0300

    modificando archivos visibles

---
+++ app/config/config.js
@@ -0,0 +1,5 @@
+const admin = require("firebase-admin");
+
+const serviceAccount = require("./ecommerce-backend-262b9-firebase-adminsdk-lh45h-6d59089bce.json");
+
+module.exports = {admin, serviceAccount}
